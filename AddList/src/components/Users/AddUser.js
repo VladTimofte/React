5807abe1bb26ec7309a78e5a96556573a7dc8f19
Mike <mihai.tincau@tracknamic.com>
2022-02-14 15:21:54 +0200

    AddList {Done}

--- AddList/src/components/Users/AddUser.js
+++ AddList/src/components/Users/AddUser.js
@@ -3,17 +3,34 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).'
+      });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredAge < 1 ) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).'
+      });
+      return;
+    }
+    if (+enteredAge < 18) {
+      setError({
+        title: 'Privacy Policy',
+        message: 'We are sorry, you are not allowed to register untill you turn on 18 years old.'
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -29,7 +46,13 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  }
+
   return (
+    <div>
+      {error && <ErrorModal title={error.title} message={error.message} onConfirmHandlerError={errorHandler}/>}
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
@@ -49,6 +72,7 @@ const AddUser = (props) => {
         <Button type="submit">Add User</Button>
       </form>
     </Card>
+    </div>
   );
 };
 
