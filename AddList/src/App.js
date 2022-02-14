293f6619a50b448dd53f1af1f0b8544d4e3840df
Mike <mihai.tincau@tracknamic.com>
2022-02-14 14:11:58 +0200

    commit

--- AddList/src/App.js
+++ AddList/src/App.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
+
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
-
 function App() {
-  const [userList, setUsersList] = useState([]);
+  const [usersList, setUsersList] = useState([]);
+
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUsersList) => {
-      return [...prevUsersList, { name: uName, age: uAge }];
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
 
   return (
     <div>
       <AddUser onAddUser={addUserHandler} />
-      <UsersList users={[userList]} />
+      <UsersList users={usersList} />
     </div>
   );
 }
